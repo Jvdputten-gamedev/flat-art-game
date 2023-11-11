5da9912f22b5190f9a513e0ef89a4417754d2a86
papydur <joostvdp92@gmail.com>
2023-11-11 17:42:39 +0100

    finish refactor

--- Scripts/AttackPatterns.gd
+++ Scripts/AttackPatterns.gd
@@ -9,21 +9,19 @@ static func get_triangle_up(from: HexCell):
 	var cells = Array()
 	var dirs = [Directions.NORTH, Directions.SOUTHEAST, Directions.SOUTHWEST]
 	for dir in dirs:
-		cells.append(from.get_adjacent(dir.cube_coords).oddq_coords)
+		cells.append(get_adjacent(from, dir.cube_coords).oddq_coords)
 	return cells
 
 static func get_triangle_down(from: HexCell):
 	var cells = Array()
 	var dirs = [Directions.NORTHEAST, Directions.SOUTH, Directions.NORTHWEST]
 	for dir in dirs:
-		cells.append(from.get_adjacent(dir.cube_coords).oddq_coords)
+		cells.append(get_adjacent(from, dir.cube_coords).oddq_coords)
 	return cells
 
 static func get_all_adjacent(from: HexCell):
 	var cells = Array()
 	for dir in Directions.DIRECTIONS:
-		cells.append(from.get_adjacent(dir.cube_coords).oddq_coords)
+		cells.append(get_adjacent(from, dir.cube_coords).oddq_coords)
 	return cells
 
-static func test(from: HexCell):
-	print(from)
