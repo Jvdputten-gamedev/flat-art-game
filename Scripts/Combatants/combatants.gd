aeb3fde1ec546990af09e2c6a6d0885a21c83f19
joost van der Putten <joostvdp92@gmail.com>
2023-11-18 14:23:07 +0100

    add state charts

--- Scripts/Combatants/combatants.gd
+++ Scripts/Combatants/combatants.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @export var player_combatant_group: Node2D
 @export var enemy_combatant_group: Node2D
+@export var _state_chart: StateChart
 
 var is_player_turn: bool
 
@@ -29,13 +30,16 @@ func _next_combatant_group():
 
 func _on_end_turn_pressed():
 	_next_combatant_group()
+	_state_chart.send_event("end_player_turn")
+
 
 func _on_enemy_turn_end():
 	_next_combatant_group()
+	_state_chart.send_event("end_enemy_turn")
+
+func _on_player_turn_state_stepped():
+	print("called from state transition to ")
 
 
-
-
-
-
-
+func _on_enemy_turn_state_stepped():
+	pass # Replace with function body.
